ec5f03aec8a981a197c67977c2e29c873d1b977b
Juliette Audema <juliette.audema@gmail.com>
2019-09-10 23:14:00 +0200

    Add index for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :events, only: [:show, :new, :create]
+  resources :events, only: [:show, :new, :create, :index]
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
